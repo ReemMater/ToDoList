9883a9b52587c5f9b2154aa221bd269c1adfbccd
ReemMater <reem.mater21@gmail.com>
2020-10-13 03:50:44 +0200

    Updates

---
+++ precache-manifest.368657790c055bb329c04504892587b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc90653cb6e9978552c27b6cadf754e6",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c28e8076693cbecb2d34",
+    "url": "/todolist/static/js/2.00a02c7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.00a02c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d4df9d9fdda153d1ff",
+    "url": "/todolist/static/js/main.e8121cf8.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
